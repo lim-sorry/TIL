dda15d9f64ee281365e216cb61cc580909a8ad83
lim-sorry <prime18k@gmail.com>
2020-08-10 17:49:07 +0900

    200810

--- startcamp/file_practice/rename_files.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.chdir(r'C:\Users\aclass\Desktop\임지성\TIL\startcamp\file_practice\dummy')
-
-filenames = os.listdir('.')
-
-for filename in filenames:
-    os.rename(filename,f'SAMSUNG_{filename}')
